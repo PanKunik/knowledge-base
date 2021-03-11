388c03e4df58b253885a597517de5875328d9c46
PanKunik <patryk.kunicki@pm.me>
2021-03-11 20:26:06 +0100

    Adding 1st design pattern called Singleton. Also removing old files and adding common project named 'ConsoleUI' where all design patterns would be used with example.

---
+++ DesignPatterns/DesignPatterns/SingletonLibrary/Singleton.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingletonLibrary
+{
+    public class Singleton
+    {
+        private static Singleton _instance { get; set; }
+        private Singleton() { }
+
+        public static Singleton GetInstance()
+        {
+            if(_instance == null)
+            {
+                _instance = new Singleton();
+            }
+
+            return _instance;
+        }
+    }
+}
